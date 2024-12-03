3fc361ca06d06af3e0648d74014c7063041c27ec
andrijaweb <andrijawebd@gmail.com>
2024-12-03 22:05:52 +0100

    Added review controller

---
+++ controllers/reviewController.js
@@ -0,0 +1,68 @@
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review)
+    return next(new AppError('No review with that ID was found!', 404));
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const updatedReview = Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (!updatedReview)
+    return next(new AppError('No review with that ID was found!', 404));
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review: updatedReview,
+    },
+  });
+});
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndDelete(req.params.id);
+
+  if (!review)
+    return next(new AppError('No review with that ID was found!', 404));
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
