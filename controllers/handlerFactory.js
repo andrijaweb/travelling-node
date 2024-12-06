ce9815490d432bb14efdc12713021298ebd9dcde
andrijaweb <andrijawebd@gmail.com>
2024-12-06 22:25:15 +0100

    Added handler factory file, added deleteOne function

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,15 @@
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const document = await Model.findByIdAndRemove(req.params.id);
+
+    if (!document)
+      return next(new AppError('No document found with that ID.'), 404);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
