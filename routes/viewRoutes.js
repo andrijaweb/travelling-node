5f677181c30bdee807f37929e4496c389f695d54
andrijaweb <andrijawebd@gmail.com>
2025-01-06 10:18:36 +0100

    Added my-tours route, added middleware createBookingCheckout for index route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,16 +4,18 @@ const {
   getTour,
   login,
   getAccount,
-  updateUserData,
+  getMyTours,
 } = require('../controllers/viewsController');
 const { isLoggedIn, protect } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tours/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, login);
 router.get('/me', protect, getAccount);
+router.get('/my-tours', protect, getMyTours);
 
 // UPDATING USER DATA WITHOUT API
 // router.post('/submit-user-data', protect, updateUserData);
